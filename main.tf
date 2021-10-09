c6d6272b517743101d69e73dba4e8ade431f3404
amulya2829 <amulya2829@gmail.com>
2021-10-09 12:52:34 -0400

    created main.tf-->provider & resource information to create the resource in Aws

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-074cce78125f09d61"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${var.env}-ubuntu-server"
+    Owner = "Dennis Astahov"
+  }
+}
